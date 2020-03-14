1139a8c8d247642f021aed5baa40db36a0d1712d
Sharon <pastula.magdalena@gmail.com>
2020-03-14 21:39:01 +0100

    Began ex 3.

--- lab1/zad3a/biblioteka.c
+++ lab1/zad3a/biblioteka.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#define FILE_BUFF_SIZE  100
+#define FILE_BUFF_SIZE  1000000
 
 char* temp_file_name = "diff_out.txt";
 
@@ -15,9 +15,18 @@ static void MT_createNewOperationUnit(int op_no) {
     op->operations = calloc(op_no, sizeof(char*));
 }
 
+static int MT_findFreeUnitSlot(main_table* mt) {
+
+    for (int i=0;i<mt->units_no;i++) {
+        if (!mt->used_units[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 void MT_createTable(main_table* mt, int size) {
-    mt ->files_seq = 0;
-    mt ->files_pairs_no = 0;
+    mt->files_seq = calloc(size, sizeof(char*));
     mt->table = calloc(size, sizeof(operation_unit*));
     mt->used_units = calloc(size, sizeof(bool));
     mt->units_no = size;
@@ -25,22 +34,16 @@ void MT_createTable(main_table* mt, int size) {
 
 void MT_defineFiles(main_table* mt, char** file_names, int files_no) {
 
-    mt->files_pairs_no = files_no/2;
-
-    if (mt->files_seq != 0) {
-        free(mt->files_seq);
-    }
-    mt->files_seq = calloc(mt->files_pairs_no, sizeof(char*));
-
-    for (int i=0;i<files_no;i++) {
+    for (int i=0;i<files_no && i<mt->units_no;i+=2) {
+        
         char* file1 = file_names[i];
         char* file2 = file_names[i+1];
         int seq_len = strlen(file1) + strlen(file2) + 2;
 
-        mt->files_seq[i] = calloc(seq_len, sizeof(char*));
-        mt->files_seq[i] = strcat(mt->files_seq[i], file1);
-        mt->files_seq[i][strlen(file1)] = ' ';
-        mt->files_seq[i] = strcat(mt->files_seq[i], file2);
+        mt->files_seq[i/2] = calloc(seq_len, sizeof(char*));
+        mt->files_seq[i/2] = strcat(mt->files_seq[i/2], file1);
+        mt->files_seq[i/2][strlen(file1)] = ' ';
+        mt->files_seq[i/2] = strcat(mt->files_seq[i/2], file2);
     }
 }
 
@@ -58,69 +61,84 @@ void MT_comparePairFromDefinedFiles(main_table* mt, int pair_no) {
     } 
 }
 
-int MT_createOperationUnitFor(main_table* mt, int file_pair_no) {
+int MT_createOperationUnitForLastPair(main_table* mt) {
     int fd = open(temp_file_name, O_RDONLY);
 
 	char* buff = calloc(FILE_BUFF_SIZE, sizeof(char));
 	int bytes_read = read(fd, buff, FILE_BUFF_SIZE);
-    
+
     if (bytes_read == -1) {
         printf("There was a problem while attempting to read diff_out.txt file.\n");
     } else if (bytes_read == 0) {
         printf("File diff_out.txt is empty.");
     }
 
-    close(fd);
-
-    int operations_no = 0;
+    int operations_no = 1;
     for (int i=1;i<bytes_read;i++) {
-        if ( buff[i]!='<' && buff[i]!='>' && buff[i]!='-' && buff[i-1]==10) {
+        if (buff[i-1]==10 && buff[i] >= '0' && buff[i] <='9') {
             operations_no++;
         }
     }
     //create operation_unit
-    mt->table[file_pair_no] = calloc(1, sizeof(operation_unit));
-    mt->table[file_pair_no]->size = operations_no;
-    mt->table[file_pair_no]->operations = calloc(operations_no, sizeof(char*));
-    mt->used_units[file_pair_no] = true;
-
-    int operation_no = 0;
-    for (int i=0;i<bytes_read-1;operation_no++) {
-        //parse content of temp file
-        int operation_size = 0;
-        while (buff[i+1]!= 10 || buff[i]=='<' || buff[i]=='>' || buff[i]=='-') {
-            operation_size++;
-        }
-        char* operation = calloc(operation_size+1, sizeof(char));
-        operation[0] = 0;
-        operation = strncat(operation, buff+i, operation_size);
-        mt->table[file_pair_no]->operations[operation_no] = operation;
-        i+=operation_size;
+    int unit_no = MT_findFreeUnitSlot(mt);
+    if (unit_no==-1){
+        printf("No place in main table to add new block\n");
+        exit(-1);
     }
 
+    mt->table[unit_no] = calloc(1, sizeof(operation_unit));
+    mt->table[unit_no]->size = operations_no;
+    mt->table[unit_no]->operations = calloc(operations_no, sizeof(char*));
+    mt->used_units[unit_no] = true;
 
+    int op_no = 0;
+    for (int i=0;i<bytes_read;op_no++) {
+        //parse content of temp file
+        int op_size = 0;
+        while (i+op_size<bytes_read && (buff[i+op_size]!= 10 || buff[i+op_size+1] < '0' || buff[i+op_size+1] >'9')) {
+            op_size++;
+            //i++;
+        }
+        char* operation = calloc(op_size+1, sizeof(char));  
+        operation = strncat(operation, buff+i, op_size);
+        mt->table[unit_no]->operations[op_no] = operation;
+        i+=op_size+1;
+    }
+
+    close(fd);
+
+    return unit_no;
+}
+
+int MT_getOperationsCounter(main_table* mt, int unit_no) {
+    if (mt != NULL){
+        if (mt->table[unit_no] != NULL){
+            return mt->table[unit_no]->size;
+        }
+    }
     return 0;
 }
 
-int MT_getOperationsCounter(operation_unit* op) {
-    return op->size;
-}
-
 void MT_deleteUnit(main_table* mt, int unit_no) {
-    if (mt->used_units[unit_no]) {
+    if (mt!=NULL && mt->used_units[unit_no]) {
         mt->used_units[unit_no] = false;
         int op_no = mt->table[unit_no]->size;
         for (int i=0;i<op_no;i++) {
-            free(mt->table[unit_no]->operations[i]);
+            if (mt->table[unit_no]->operations[i]!=NULL){
+                free(mt->table[unit_no]->operations[i]);    
+            }
         }
         free(mt->table[unit_no]->operations);
         free(mt->table[unit_no]);
+        mt->used_units[unit_no] = false;
     }
 }
 
 void MT_deleteOperation(main_table* mt, int unit_no, int op_no) {
-    if (mt->used_units[unit_no]) {
-        free(mt->table[unit_no]->operations[op_no]);
+    if (mt!=NULL && mt->used_units[unit_no]) {
+        if (mt->table[unit_no]->operations[op_no] != NULL) {
+            free(mt->table[unit_no]->operations[op_no]);
+        }
     }
 }
 
--- lab1/zad3a/test.c
+++ lab1/zad3a/test.c
@@ -4,40 +4,145 @@
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
+#include <sys/times.h>
+#include <stdbool.h>
 
-#include "../zad2/biblioteka.h"
+#include "biblioteka.h"
+
+int execute_command(char* command, main_table* mt, char** argv) {
+	if (strcmp(command, "create_table")==0) {
+		main_table mt;
+		int mt_size = atoi(argv[0]);
+		MT_createTable(&mt, mt_size);
+		return 1;
+	}
+	else if (strcmp(command, "compare_pairs")==0) {
+		int pairs_no = atoi(argv[0]);
+		
+		MT_defineFiles(mt, argv+1, pairs_no*2);
+	
+		for (int i=0;i<pairs_no;i++) {
+			MT_comparePairFromDefinedFiles(mt, i);
+			MT_createOperationUnitForLastPair(mt);
+		} 
+		
+		return pairs_no*2+1;
+	}
+	else if (strcmp(command, "remove_block")==0) {
+		int unit_no = atoi(argv[0]);
+		MT_deleteUnit(mt, unit_no);
+		return 1;
+	}
+	else if (strcmp(command, "remove_operation")==0) {
+		int unit_no = atoi(argv[0]);
+		int op_no = atoi(argv[1]);
+		MT_deleteOperation(mt, unit_no, op_no);
+		return 2;
+	}
+	else if (strcmp(command, "add_block")==0) {
+
+		MT_createOperationUnitForLastPair(mt);
+
+		return 0;
+	}
+	else if (strcmp(command, "add_and_delete")==0) {
+		int units_no = atoi(argv[0]);
+		int steps_no = atoi(argv[1]);
+		
+		if(units_no > mt->units_no) {
+			printf("Number of block to add and delete bigger than table size.\n");
+			exit(-1);
+		}
+
+		int* units_nos = calloc(units_no,sizeof(int));
+
+		for (int i=0;i<steps_no;i++) {
+			for (int j=0;j<units_no;j++) {
+				units_nos[j] = MT_createOperationUnitForLastPair(mt);
+			}
+
+			for(int j=0;j<units_no;j++){
+				MT_deleteUnit(mt, units_nos[j]);
+			}
+
+		}
+		return 2;
+	}
+	else if (strcmp(command, "get_ops_no")==0) {
+		int unit_no = atoi(argv[0]);
+		int op_no = MT_getOperationsCounter(mt, unit_no);
+
+		printf("Op no for block %d: %d\n", unit_no, op_no);
+
+		return 1;
+	}
+	else {
+		printf("Not known command.\n");
+		printf("Command: %s \n", command);
+		exit(-1);
+		return 0;
+	}
+}
+
+void printfTimes(clock_t r_time_start, clock_t r_time_end, struct tms* su_time_start, struct tms* su_time_end) {
+	printf("Real time: %f s\n", ((double)r_time_end-r_time_start)/sysconf(_SC_CLK_TCK));
+	printf("User time: %f s\n", ((double)su_time_end->tms_utime-su_time_start->tms_utime)/sysconf(_SC_CLK_TCK));
+	printf("System time: %f s\n", ((double)su_time_end->tms_stime-su_time_start->tms_stime)/sysconf(_SC_CLK_TCK));
+}
+
+bool checkArgs(int argc, char** argv) {
+	if (argc == 1) {
+		printf("No arguments.\n");
+		return false;
+	}
+	if (argc < 2) {
+		printf("Not enough arguments.\n");
+		return false;
+	}
+	if (atoi(argv[1]) <= 0) {
+		printf("Table size should be bigger than 0.\n");
+		return false;
+	}
+
+	return true;
+}
 
 int main(int argc, char **argv) {
 
-	if (argc == 1) {
-		printf("Za mało argumentów.\n");
-		exit(-1);
-	}
-	if (argc < 3) {
-		printf("Za mało argumentów\n");
-		exit(-1);
-	}
+	if (!checkArgs(argc, argv))		exit(-1);
 
 	int mt_size = atoi(argv[1]);
-	time_t sys_time;
-	time_t real_time;
-	time_t user_time;
 
-	if (strcmp(argv[2], "create_table")==0) {
-		main_table mt;
-		MT_createTable(&mt, mt_size);
-	}
+	struct tms sys_us_time_start;
+	clock_t real_time_start = times(&sys_us_time_start);
+	
+	main_table mt;
+	MT_createTable(&mt, mt_size);
 
-	if (strcmp(argv[2], "compare_pairs")==0) {
+	struct tms sys_us_time_end;
+	clock_t real_time_end;
+	
+	real_time_end = times(&sys_us_time_end);
+
+	printf("Times of table creating:\n");
+	printfTimes(real_time_start, real_time_end, &sys_us_time_start, &sys_us_time_end);
+
+	int index = 2;
+	while (index < argc) {
+		struct tms sys_us_time_start_w;
+		clock_t real_time_start_w = times(&sys_us_time_start_w);
+		char* command = argv[index];
+		index++;
+		index += execute_command(command, &mt, argv+index);
 		
-		for (int i=3;i<argc;i++) {
-			;
-		}
+		struct tms sys_us_time_end_w;
+		clock_t real_time_end_w = times(&sys_us_time_end_w);
 
+		printf("Times taken for command: %s\n", command);
+		printfTimes(real_time_start_w, real_time_end_w, &sys_us_time_start_w, &sys_us_time_end_w);
 	}
 
-
-	real_time = user_time/CLOCKS_PER_SEC;
+	return 0;
 
 }
 
