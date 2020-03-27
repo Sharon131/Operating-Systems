1efd07140e43642431f4d46d429f48c34fe80001
Sharon <pastula.magdalena@gmail.com>
2020-03-27 09:58:55 +0100

    Added reading matrices files.

--- PastulaMagdalena/cw03/zad2/child.c
+++ PastulaMagdalena/cw03/zad2/child.c
@@ -3,13 +3,34 @@
 #include <unistd.h>
 //#include "macierz.h"
 
+void read_matrix_from(char* file_name) {
+    FILE* fp = fopen(file_name, "r");
+
+    char* buff = NULL;
+    int buff_size = 0;
+    getline(&buff,&buff_size,fp);
+
+    int** matrix = calloc(0,sizeof(int*));
+
+    fclose(file_name);
+}
+
+void multiply() {
+
+}
+
 int main(int argc, char** argv) {
     //jako argumenty nazwy plików z macierzami już
     //a nie początkowy plik
     //działanie procesów potomnych
     //otwieranie plików z macierzami i odczytanie ich
 
+    char* mfile1 = argv[1];
+    char* mfile2 = argv[2];
+    char* exfile = argv[3];
+
     printf("Program 'child', pid: %d\n", (int)getpid());
+    printf("Args:\n%s%s%s\n", argv[0], argv[1], argv[2]);
 
     //zamknięte plików z macierzami
     return 0;
--- PastulaMagdalena/cw03/zad2/main.c
+++ PastulaMagdalena/cw03/zad2/main.c
@@ -6,15 +6,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void check_argc(int argc) {
-    if (argc == 1) {
-        printf("Program executed with no arguments.\n");
-        exit(-1);
-    } else if (argc == 2) {
-        printf("Number of child processes was not given.\n");
-        exit(-1);
-    }
-}
+void check_argc(int argc);
 
 
 int main(int argc, char** argv) {
@@ -25,24 +17,31 @@ int main(int argc, char** argv) {
     char* file_name = argv[arg_indx++];
     int children_no = atoi(argv[arg_indx]);
 
+    // Read files names
+    char *mfile1 = NULL, *mfile2 = NULL, *exfile = NULL;
+    size_t mfile1_size = 0, mfile2_size = 0, exfile_size = 0;
+
+    FILE* fp = fopen(file_name,"r");
+
+    getline(&mfile1,&mfile1_size, fp);
+    getline(&mfile2,&mfile2_size, fp);
+    getline(&exfile, &exfile_size, fp);
+
+    fclose(fp);
+    // Reding file names end
+    
     pid_t* children_pids = calloc(children_no, sizeof(pid_t));
 
     int indx = 0;
     children_pids[indx++] = fork();
 
     while(children_pids[indx-1]!= 0 && indx<children_no) {
-        //if (children_pids==NULL || children_pids[indx-1] == 0) {
-        //    printf("Child process exiting while loop.\n");
-        //    break;
-        //}
-        //else{
         children_pids[indx] = fork();
-        //}
         indx++;
     }
 
     if(children_pids[indx-1]==0) {
-        execvp("./child", NULL);
+        execlp("./child", mfile1, mfile2, exfile, NULL);
     } else {
         printf("Program 'main', pid: %d\n", (int)getpid());
     }
@@ -50,3 +49,20 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+
+void check_argc(int argc) {
+    if (argc == 1) {
+        printf("Program executed with no arguments.\n");
+        exit(-1);
+    } else if (argc == 2) {
+        printf("Number of child processes was not given.\n");
+        exit(-1);
+    } /* else if (argc == 3) {
+        printf("Time limit for child process was not given.\n");
+        exit(-1);
+    } else if (argc == 4) {
+        printf("Craeting result file mode not specified.\n");
+        exit(-1);
+    }
+    */
+}
